a0825e29a04fbc0d35b51058236493eb9716f1d9
Alfi Dev <alvianfebri2023@gmail.com>
2025-08-10 14:31:00 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,19 @@
+export default {
+  telegramBotToken: 'BOT_TOKEN', // Bot token dari @BotFather
+  beta: 'YOURAPIKEY', // Apikey betabotz
+
+  // === CPANEL SETUP ===
+  plta: 'plta_', // plta cpanel
+  pltc: 'pltc_', // pltc cpanel
+  domain: 'url', // URL PANEL tanpa / di akhir
+  egg: '15',
+  plta2: 'plta_', // plta 2 cpanel ini maksudnya server 2
+  pltc2: 'pltc', // pltc 2 cpanel
+  domain2: 'url2', // url panel 2
+
+
+  // === OWNER SETUP ===
+  owner: [
+    7905599118 // <- ganti dengan Telegram ID kamu
+  ]
+}
